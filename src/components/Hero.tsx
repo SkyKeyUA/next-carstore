fbbab52b9875671e6f00d7c02aaa3f0070cd140a
SkyKeyUA <veremiienkoov96@gmail.com>
2023-07-20 13:55:35 +0300

    Navbar and first block were finished

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,7 @@
 /** @format */
 'use client';
 
+import Image from 'next/image';
 import { CustomButton } from './CustomButton';
 
 export const Hero = () => {
@@ -17,6 +18,12 @@ export const Hero = () => {
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay"></div>
+        </div>
       </div>
     </div>
   );
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@
 
 export { Hero } from './Hero';
 export { CustomButton } from './CustomButton';
+export { Navbar } from './Navbar';
+export { Footer } from './Footer';
