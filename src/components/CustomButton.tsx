7ac56c844eb27c6f19d571401bcbd830bd2c149b
SkyKeyUA <veremiienkoov96@gmail.com>
2023-07-20 12:42:43 +0300

    types CustomButtonProps was created

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -1,12 +1,16 @@
 /** @format */
-'use client';
 
 import React from 'react';
 import Image from 'next/image';
+import { CustomButtonProps } from '@/types';
 
-export const CustomButton = () => {
+export const CustomButton = ({ title, containerStyles, handleClick }: CustomButtonProps) => {
   return (
-    <button disabled={false} type={'button'} className={`custom-btn`} onClick={() => {}}>
+    <button
+      disabled={false}
+      type={'button'}
+      className={`custom-btn ${containerStyles}`}
+      onClick={() => {}}>
       <span className={`flex-1`}>title</span>
     </button>
   );
