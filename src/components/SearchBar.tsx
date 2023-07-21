ab8528e4e837384665c59f3a887c9491376a3244
SkyKeyUA <veremiienkoov96@gmail.com>
2023-07-21 19:25:28 +0300

    router was added

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -5,11 +5,39 @@ import React from 'react';
 import { SearchManufacturer } from './SearchManufacturer';
 import { SearchButton } from './SearchButton';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 export const SearchBar = () => {
   const [manufacturer, setManufacturer] = React.useState('');
   const [model, setModel] = React.useState('');
-  const handleSearch = () => {};
+  const router = useRouter();
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (manufacturer === '' && model === '') {
+      return alert('Please fill in the search bar');
+    }
+
+    updateSearchParams(model.toLocaleLowerCase(), manufacturer.toLocaleLowerCase());
+  };
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    if (model) {
+      searchParams.set('model', model);
+    } else {
+      searchParams.delete('model');
+    }
+    if (manufacturer) {
+      searchParams.set('manufacturer', manufacturer);
+    } else {
+      searchParams.delete('manufacturer');
+    }
+
+    const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
+    router.push(newPathname);
+  };
   return (
     <form className="searchbar" onSubmit={handleSearch}>
       <div className="searchbar__item">
